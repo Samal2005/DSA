13ab577b8f227c6092eace96ee9f352b244840d4
Samal2005 <subhalaxmisamal12345@gmail.com>
2026-07-13 15:48:21 +0530

    Time: 3 ms (75.34%) | Memory: 13.6 MB (6.63%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -4,11 +4,11 @@ public:
         map<char,int> mp={{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
         int ans=0;
         for(int i=0;i<s.size();i++){
-            if (mp[s[i+1]]>mp[s[i]]){
-                ans-=mp[s[i]];
+            if (mp[s[i+1]]<=mp[s[i]]){
+                ans+=mp[s[i]];
             }
             else{
-                ans+=mp[s[i]];
+                ans-=mp[s[i]];
             }
         }
 return ans;
